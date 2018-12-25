34eb60445032369b0f024f07a7917d652175f6b9
h4x0r139 <h4x0r_001@163.com>
2018-12-25 15:21:07 +0800

    webview 增加title 回调

--- app_lib/android-lib-base/src/main/java/cn/yinxm/lib/activity/WebViewActivity.java
+++ app_lib/android-lib-base/src/main/java/cn/yinxm/lib/activity/WebViewActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
@@ -16,6 +15,7 @@ import android.webkit.WebViewClient;
 import cn.yinxm.lib.R;
 import cn.yinxm.lib.utils.log.LogUtil;
 
+
 public class WebViewActivity extends AppCompatActivity {
 
     public static final String DEFAULT_ENCODING = "UTF-8";
@@ -24,7 +24,6 @@ public class WebViewActivity extends AppCompatActivity {
      * 通过判断url中的参数，来关闭页面
      */
     public static final String CLOSE_URL = "close_url";
-    private Toolbar mToolbar;
     private WebView mWebView;
 
     private String mCloseUrl = "";
@@ -34,10 +33,6 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_web_view);
-
-        mToolbar = findViewById(R.id.tb_toolbar);
-
-        setSupportActionBar(mToolbar);
 
         mWebView = findViewById(R.id.webView);
 
@@ -55,41 +50,36 @@ public class WebViewActivity extends AppCompatActivity {
     private void initWebViewSetting(WebView webView) {
 
         WebSettings webSettings = webView.getSettings();
-        //5.0以上开启混合模式加载
+        // 5.0以上开启混合模式加载
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
-        //允许js代码
+        // 允许js代码
         webSettings.setJavaScriptEnabled(true);
-        //允许SessionStorage/LocalStorage存储
+        // 允许SessionStorage/LocalStorage存储
         webSettings.setDomStorageEnabled(true);
-        //禁用放缩
+        // 禁用放缩
         webSettings.setDisplayZoomControls(false);
         webSettings.setBuiltInZoomControls(false);
-        //禁用文字缩放
+        // 禁用文字缩放
         webSettings.setTextZoom(100);
-        //10M缓存，api 18后，系统自动管理。
+        // 10M缓存，api 18后，系统自动管理。
         webSettings.setAppCacheMaxSize(10 * 1024 * 1024);
-        //允许缓存，设置缓存位置
+        // 允许缓存，设置缓存位置
         webSettings.setAppCacheEnabled(true);
         webSettings.setAppCachePath(getApplication().getDir("appcache", 0).getPath());
-        //允许WebView使用File协议
+        // 允许WebView使用File协议
         webSettings.setAllowFileAccess(true);
-        //不保存密码
+        // 不保存密码
         webSettings.setSavePassword(false);
-        //设置UA
+        // 设置UA
 //        webSettings.setUserAgentString(webSettings.getUserAgentString() + " kaolaApp/" + AppUtils.getVersionName());
-        //移除部分系统JavaScript接口
+        // 移除部分系统JavaScript接口
 //        KaolaWebViewSecurity.removeJavascriptInterfaces(webView);
-        //自动加载图片
+        // 自动加载图片
         webSettings.setLoadsImagesAutomatically(true);
-
-//        NORMAL：正常显示，没有渲染变化。
-//        SINGLE_COLUMN：把所有内容放到WebView组件等宽的一列中。
-//        NARROW_COLUMNS：可能的话，使所有列的宽度不超过屏幕宽度。
-//        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
 
         // 设置不用系统浏览器打开,直接显示在当前Webview
         webView.setWebViewClient(new WebViewClient() {
@@ -103,7 +93,6 @@ public class WebViewActivity extends AppCompatActivity {
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
                 LogUtil.d("onPageStarted ——》 " + url);
-                mToolbar.setTitle(url);
             }
 
             @Override
@@ -122,8 +111,22 @@ public class WebViewActivity extends AppCompatActivity {
         });
         // 设置WebChromeClient类
         webView.setWebChromeClient(new WebChromeClient() {
-
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                // 设置标题
+            }
         });
+
+        if (Build.VERSION.SDK_INT <= 16) {
+            try {
+                webView.removeJavascriptInterface("searchBoxJavaBridge_");
+                webView.removeJavascriptInterface("accessibility");
+                webView.removeJavascriptInterface("accessibilityTraversal");
+            } catch (Exception e) {
+                LogUtil.e(e);
+            }
+        }
 
     }
 
